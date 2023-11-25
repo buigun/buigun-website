44699d0320a552e45066cd5300ce8a5542fa9d1b
Budi Gunawan <budiindra.g@gmail.com>
2023-11-25 19:14:18 +0700

    adjust homepage header

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Buigun, Software Engineer and Writer',
-  tagline: 'Hello! My name is Budi. I love to write codes and words',
+  title: 'Buigun Website',
+  tagline: 'I love to write codes and words',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -173,6 +173,7 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      forceDarkMode: true,
     }),
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import { TypeAnimation } from 'react-type-animation';
+import HeroImg from '../../static/img/header-img.svg';
 
 import styles from './index.module.css';
+import 'animate.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className='container'>
-        <h1 className='hero__title'>{siteConfig.title}</h1>
-        <p className='hero__subtitle'>{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className='button button--secondary button--lg'
-            to='/docs/intro'
-          >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+    <header className={clsx('hero', styles.heroBanner)}>
+      <div className={clsx('container')}>
+        <div className={clsx('row')} style={{ alignItems: 'center' }}>
+          <div className={clsx('col col--7')}>
+            <h1 className='hero__title'>
+              {`Buigun, `}
+              <br />
+              <TypeAnimation
+                sequence={[
+                  'Software Engineer',
+                  2000,
+                  'Frontend Developer',
+                  2000,
+                  'Writer',
+                  2000,
+                ]}
+                repeat={Infinity}
+              />
+            </h1>
+            <p className='hero__subtitle'>
+              Hello world! 👋🏻 My name is Budi Indra Gunawan{' '}
+              <strong>{`(buigun). `}</strong>I have over two years of experience
+              in web development, mostly in frontend. Adeptly utilizing
+              JavaScript libraries and frameworks such as
+              <strong>{` Vue JS, React JS, and Express JS. `}</strong>I love to
+              write codes and words.
+              <br />
+              <br />
+              Welcome to my website 😆
+            </p>
+          </div>
+          <div className={clsx('col col--5 animate__animated animate__zoomIn')}>
+            <HeroImg />
+          </div>
         </div>
       </div>
     </header>
